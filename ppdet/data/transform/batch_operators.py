75492cf8998bc2dcb08cf8a1d24739a2bcb4204b
WasedaMagina <koushin@toki.waseda.jp>
2020-12-09 15:23:16 +0800

    update

--- ppdet/data/transform/batch_operators.py
+++ ppdet/data/transform/batch_operators.py
@@ -395,14 +395,15 @@ class Gt2YoloTarget_1vN(BaseOperator):
                 gt_labels = gt_class[assigned_result]
                 
                 reg_target = self._get_reg_target(bboxes=anchor_by_level,gt_bboxes=gt_bboxes,stride=downsample_ratio)
-                target[:,0:4,...] = reg_target.reshape((len(mask),grid_w,grid_h,4)).transpose(0,3,2,1)
+                target[:,0:4,...] = reg_target.reshape((grid_w,grid_h,len(mask),4)).transpose(2,3,1,0)
                 obj_target = np.zeros((anchor_by_level.shape[0]),dtype=np.float32)
                 obj_target[pos_idx] = 1
-                target[:,4,...] = obj_target.reshape((len(mask),grid_w,grid_h))
-                target[:,5,...] = obj_target.reshape((len(mask),grid_w,grid_h))
+                target[:,4,...] = obj_target.reshape((grid_w,grid_h,len(mask))).transpose(2,0,1)
+                target[:,5,...] = obj_target.reshape((grid_w,grid_h,len(mask))).transpose(2,0,1)
                 label_target = np.eye(self.num_classes)[gt_labels]
+                label_target[assigned_result<0] = 0
                 #shape = target[:,6:,...].shape
-                target[:,6:,...] = label_target.reshape((len(mask),grid_w,grid_h,80)).transpose(0,3,2,1)
+                target[:,6:,...] = label_target.reshape((grid_w,grid_h,len(mask),80)).transpose(2,3,1,0)
                 #assigned_result = assigned_result.reshape(len(mask),grid_w,grid_h)
                 #target_gt = gt_bbox_filted[assigned_result]
             
